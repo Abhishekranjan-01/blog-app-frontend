8942e49281ea9c2c87ed25a1d5a785ad07c562fe
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2025-04-25 19:56:41 +0530

    final v-1.0

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+export const constants = {
+  APP_NAME: "Tech World",
+  // API_URL: "https://blog-backend-1.onrender.com/api/v1",
+  //   API_URL: "http://localhost:4000/api/v1",
+  // API_URL: "https://blog-backend-1.onrender.com/api/v1",
+  // API_URL: "https://blog-backend-1.onrender.com/api/v1",
+};
