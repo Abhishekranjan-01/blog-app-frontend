2604bebd7a869932703771cbd6d91f2075bc031a
Abhishekranjan-01 <abhishekranjan7991@outlook.com>
2025-04-26 09:31:30 +0530

    new domain added with https

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const constants = {
   APP_NAME: "Tech World",
   // API_URL: "https://blog-backend-1.onrender.com/api/v1",
-    API_URL: "http://13.203.3.111:4001/",
+  API_URL: "http://blog.eklavyahometuition.in/",
   // API_URL: "https://blog-backend-1.onrender.com/api/v1",
   // API_URL: "https://blog-backend-1.onrender.com/api/v1",
 };
